f9e2a8dfb33d71fc0add4773ba65df391b4df900
HR RPS <HR RPS@DESKTOP-JD36S8B>
2024-06-18 16:13:32 +0330

    Sp added in solution

---
+++ ChatApp.Api/GetAllContacts.sql
@@ -0,0 +1,19 @@
+﻿USE [ChatApp]
+GO
+/****** Object:  StoredProcedure [dbo].[GetAllContacts]    Script Date: 6/18/2024 4:03:25 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[GetAllContacts] 
+AS
+BEGIN
+	SELECT C.[Id]
+	,C.[ContactName]
+	,P.[Id] AS ProfilePhoto
+	FROM Contacts AS C
+	JOIN ProfilePhotos AS P
+	ON P.UserId = C.UserId
+	ORDER BY C.[ContactName]
+END
